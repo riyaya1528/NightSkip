3775f1b582cc43f2d11ad75da55a7cf78ebb53e4
riyaya1528 <46711875+riyaya1528@users.noreply.github.com>
2021-08-16 12:45:32 +0900

    Add files via upload

--- src/main/java/riyaya1528/paper/nightskip/NightSkip.java
+++ src/main/java/riyaya1528/paper/nightskip/NightSkip.java
@@ -6,6 +6,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 public final class NightSkip extends JavaPlugin implements Listener {
     public static boolean NightSkip = true;
     public static int PlayerSleepNeed;
+    public static String SleepNowMessage;
+    public static String SleepSuccessMessage;
 
     @Override
     public void onEnable() {
@@ -13,6 +15,9 @@ public final class NightSkip extends JavaPlugin implements Listener {
         saveDefaultConfig();
         NightSkip = getConfig().getBoolean("NightSkip");
         PlayerSleepNeed = getConfig().getInt("PlayerSleepNeed");
+        SleepNowMessage = getConfig().getString("SleepNowMessage");
+        SleepSuccessMessage = getConfig().getString("SleepSuccessMessage");
+
 
         System.out.println("[NightSkip]プラグインが有効になりました");
     }
--- src/main/java/riyaya1528/paper/nightskip/Sleep.java
+++ src/main/java/riyaya1528/paper/nightskip/Sleep.java
@@ -1,29 +1,68 @@
 package riyaya1528.paper.nightskip;
 
-import io.papermc.paper.event.player.PlayerDeepSleepEvent;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
 
+import static org.bukkit.Bukkit.getServer;
+
 public class Sleep implements Listener {
-    int sleepPlayer = 0;
+    int sleepPlayers = 0;
+    String SleepNowMessage = NightSkip.SleepNowMessage;
 
     @EventHandler
     public void onSleep(PlayerBedEnterEvent e) {
-        sleepPlayer = sleepPlayer + 1;
+        if(!isDay(e.getPlayer().getWorld().getName())) {
+            sleepPlayers = sleepPlayers + 1;
+            int SleepNowPlayers = NightSkip.PlayerSleepNeed - sleepPlayers;
+            SleepNowMessage = NightSkip.SleepNowMessage.replace("[プレイヤーの数]",String.valueOf(SleepNowPlayers));
 
-        if(sleepPlayer >= NightSkip.PlayerSleepNeed) {
-            e.getPlayer().getWorld().setTime(1000);
+            if(SleepNowPlayers <= 0) {
+                for (Player p : e.getPlayer().getWorld().getPlayers()) {
+                    p.sendMessage(NightSkip.SleepSuccessMessage);
+                }
+            } else {
+                for (Player p : e.getPlayer().getWorld().getPlayers()) {
+                    p.sendMessage(SleepNowMessage);
+                }
+            }
+
+            if(sleepPlayers >= NightSkip.PlayerSleepNeed) {
+                e.getPlayer().getWorld().setTime(1000);
+                sleepPlayers = 0;
+            }
         }
     }
     @EventHandler
     public void onUnSleep(PlayerBedLeaveEvent e) {
-        sleepPlayer = sleepPlayer - 1;
+        if(!isDay(e.getPlayer().getWorld().getName())) {
+            sleepPlayers = sleepPlayers - 1;
 
-        if(sleepPlayer <= 0) {
-            sleepPlayer = 0;
+            if (sleepPlayers <= 0) {
+                sleepPlayers = 0;
+            }
+
+            int SleepNowPlayers = NightSkip.PlayerSleepNeed - sleepPlayers;
+            SleepNowMessage = NightSkip.SleepNowMessage.replace("[プレイヤーの数]", String.valueOf(SleepNowPlayers));
+
+            if(SleepNowPlayers <= 0) {
+                for (Player p : e.getPlayer().getWorld().getPlayers()) {
+                    p.sendMessage(NightSkip.SleepSuccessMessage);
+                }
+            } else {
+                for (Player p : e.getPlayer().getWorld().getPlayers()) {
+                    p.sendMessage(SleepNowMessage);
+                }
+            }
         }
+    }
+
+    public boolean isDay(String worldname) {
+
+        long time = getServer().getWorld(worldname).getTime();
+        return time < 12300 || time > 23850;
+
     }
 }
